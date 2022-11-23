9e35280d83ee31b5d05658532f043dfe372cb94a
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-24 01:07:00 +0600

    server setup, readme updated

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+console.log(process.env.DB_user);
 
 const app = express();
 
 /* <===============Middlewares============> */
+
 /** MiddleWare
  * cors()
  * express.json body parser to fetch json body
  */
+
 app.use(cors());
 app.use(express.json());
 /* <===============Middlewares end============> */
 
-/* <===============Intial API============> */
+/* <===============Root API============> */
 app.get("/", (req, res) => {
   res.json({ message: "Resales Utopia is running" });
 });
+
+app.get("/names", (req, res) => {
+  res.json({ name: "Dabasish Das Joy" });
+});
 /* <===============Intial API end============> */
 
+/* <===============Server Connection============> */
 app.listen(port, () => {
   console.log("Resales Utopia Server is running on port: ", port);
 });
