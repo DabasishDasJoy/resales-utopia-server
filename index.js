68386eaa5264d5280d3967de4f451c56507e4494
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-24 01:11:05 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
+
 const port = process.env.PORT || 5000;
 
 console.log(process.env.DB_user);
