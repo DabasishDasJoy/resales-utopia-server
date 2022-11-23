11d12a8ced8fe9aad4e1ceee91efc975244e49ab
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-24 01:31:54 +0600

    database connection stablished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 var jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -29,8 +30,26 @@ app.get("/names", (req, res) => {
   res.json({ name: "Dabasish Das Joy" });
 });
 /* <===============Intial API end============> */
+/* <===============Database============> */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mj0nqa8.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+/* <===============Database end============> */
 
 /* <===============Server Connection============> */
 app.listen(port, () => {
+  client.connect((err) => {
+    if (err) {
+      console.log("Database ERROR: ", err);
+    } else {
+      console.log("Database Connected");
+    }
+  });
   console.log("Resales Utopia Server is running on port: ", port);
 });
